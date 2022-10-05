5648f2eda82c2b4d36c4f8c64be94f8bcdee86ad
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-05 03:24:12 +0000

    Use of a simple function

---
+++ hello.js
@@ -0,0 +1,11 @@
+// Simple hello function
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+// Improved hello function
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
